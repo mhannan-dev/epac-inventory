1cf9b0d33681b649c14fae262d3c56598cb95ce1
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2021-03-04 18:28:05 +0600

    sql

---
+++ shop_pos (5).sql
@@ -0,0 +1,519 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 04, 2021 at 12:27 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop_pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`, `created_by`, `updated_by`, `status`) VALUES
+(1, 'Rice', '2021-02-18 02:44:50', '2021-02-27 09:19:26', 8, 8, 'active'),
+(2, 'Oil', '2021-02-18 02:44:57', '2021-02-27 09:19:08', 8, 8, 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile_no` varchar(50) NOT NULL,
+  `address` text,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `created_by`, `updated_by`, `email`, `mobile_no`, `address`, `updated_at`, `created_at`) VALUES
+(1, 'Abdullah Al Mamun', 8, 8, 'admin@bank.com', '01744894452', 'Dhaka', '2021-02-18 01:16:10', '2021-02-18 01:15:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL,
+  `invoice_no` int(50) NOT NULL,
+  `date` date NOT NULL,
+  `description` text,
+  `status` tinyint(4) DEFAULT '0' COMMENT '0 = Pending, 1 = Approved',
+  `approved_by` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `invoice_no`, `date`, `description`, `status`, `approved_by`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 1, '2021-02-18', 'Rice 20 litre', 1, 8, 8, NULL, '2021-02-18 02:17:34', '2021-02-18 02:17:49'),
+(2, 2, '2021-02-18', 'Mota chal', 1, 8, 8, NULL, '2021-02-18 02:50:36', '2021-02-18 02:51:04'),
+(3, 3, '2021-02-18', 'Mota chal	98 kg', 1, 8, 8, NULL, '2021-02-18 02:52:36', '2021-02-18 02:52:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_details`
+--
+
+CREATE TABLE `invoice_details` (
+  `id` int(11) NOT NULL,
+  `invoice_id` int(50) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `selling_qty` double NOT NULL,
+  `unit_price` double NOT NULL,
+  `selling_price` double NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `invoice_details`
+--
+
+INSERT INTO `invoice_details` (`id`, `invoice_id`, `category_id`, `product_id`, `date`, `selling_qty`, `unit_price`, `selling_price`, `status`, `updated_at`, `created_at`) VALUES
+(1, 1, 1, 2, '2021-02-18', 20, 180, 3600, 1, '2021-02-18 02:17:34', '2021-02-18 02:17:34'),
+(2, 2, 1, 1, '2021-02-18', 10, 110, 1100, 1, '2021-02-18 02:50:36', '2021-02-18 02:50:36'),
+(3, 3, 1, 1, '2021-02-18', 98, 100, 9800, 1, '2021-02-18 02:52:36', '2021-02-18 02:52:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `invoice_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `paid_status` varchar(50) NOT NULL,
+  `paid_amount` double NOT NULL,
+  `due_amount` double NOT NULL,
+  `total_amount` double NOT NULL,
+  `discount_amount` float DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`id`, `invoice_id`, `customer_id`, `paid_status`, `paid_amount`, `due_amount`, `total_amount`, `discount_amount`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'full_paid', 3500, 0, 3500, 100, '2021-02-18 02:17:34', '2021-02-18 02:17:34'),
+(2, 2, 1, 'full_paid', 1000, 0, 1000, 100, '2021-02-18 02:50:36', '2021-02-18 02:50:36'),
+(3, 3, 1, 'full_paid', 9800, 0, 9800, 0, '2021-02-18 02:52:36', '2021-02-18 02:52:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_details`
+--
+
+CREATE TABLE `payment_details` (
+  `id` int(11) NOT NULL,
+  `invoice_id` int(20) NOT NULL,
+  `current_paid_amount` float NOT NULL,
+  `date` date DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `payment_details`
+--
+
+INSERT INTO `payment_details` (`id`, `invoice_id`, `current_paid_amount`, `date`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, 1, 3500, '2021-02-18', NULL, '2021-02-18 02:17:34', '2021-02-18 02:17:34'),
+(2, 2, 1000, '2021-02-18', NULL, '2021-02-18 02:50:36', '2021-02-18 02:50:36'),
+(3, 3, 9800, '2021-02-18', NULL, '2021-02-18 02:52:36', '2021-02-18 02:52:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `category_id` bigint(200) UNSIGNED DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `unit_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `name`, `created_by`, `updated_by`, `created_at`, `updated_at`, `supplier_id`, `unit_id`, `quantity`) VALUES
+(1, 1, 'Mota chal', 8, 8, '2021-02-18 02:12:23', '2021-02-18 06:00:14', 2, NULL, 195),
+(2, 2, 'Oil', 8, 8, '2021-02-18 02:13:28', '2021-02-18 03:32:42', 2, NULL, 249),
+(4, 1, 'XYZ name', 8, 8, '2021-02-18 04:43:37', '2021-02-18 06:00:22', 2, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `buying_qty` double NOT NULL,
+  `unit_price` int(11) NOT NULL,
+  `buying_price` double NOT NULL,
+  `description` text,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 = Pending, 1 = Approved',
+  `date` date NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `unit_id` int(11) DEFAULT NULL,
+  `size_weight` varchar(100) DEFAULT NULL,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`id`, `category_id`, `supplier_id`, `buying_qty`, `unit_price`, `buying_price`, `description`, `status`, `date`, `created_by`, `product_id`, `unit_id`, `size_weight`, `updated_at`, `created_at`) VALUES
+(2, 1, 2, 200, 100, 20000, 'Mota chal', 1, '2021-02-18', 8, 1, 2, NULL, '2021-02-18 08:47:37', '2021-02-18 08:47:37'),
+(4, 2, 2, 10, 150, 1500, 'Soyabean Oil', 0, '2021-02-18', 8, 2, 1, NULL, '2021-02-18 09:35:05', '2021-02-18 09:35:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `slug` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `slug`) VALUES
+(1, 'Admin', 'admin'),
+(2, 'User', 'user'),
+(3, 'Admin', 'admin'),
+(4, 'User', 'user'),
+(5, 'Admin', 'admin'),
+(6, 'User', 'user'),
+(7, 'Admin', 'admin'),
+(8, 'User', 'user'),
+(9, 'Admin', 'admin'),
+(10, 'User', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `mobile_no` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` text NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `mobile_no`, `email`, `address`, `status`, `created_by`, `updated_by`, `updated_at`, `created_at`) VALUES
+(2, 'Square', NULL, 'securepass@admin.com', 'Dhaka update', 'active', 8, 8, '2021-02-18 01:14:47', '2021-02-18 01:14:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`id`, `name`, `status`, `created_by`, `updated_by`, `updated_at`, `created_at`) VALUES
+(1, 'Litre', 'active', 8, 8, '2021-02-18 02:45:55', '2021-02-18 02:45:30'),
+(2, 'Kg', 'active', 8, NULL, '2021-02-18 02:46:50', '2021-02-18 02:46:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `role_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '2',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'default.png',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `name`, `username`, `email`, `mobile`, `gender`, `address`, `password`, `image`, `remember_token`, `created_at`, `updated_at`) VALUES
+(8, '1', 'Muhammad Hannan Ali', 'admin', 'mdhannan.info@gmail.com', '01744894452', 'Male', 'Dhaka', '$2y$10$/hxhnQOSwCGO.loRlBoxiOTz2DOxZaqbWkx/hgQHtWR/LWuliJud.', '/upload/user/1608013066.jpg', 'DRmeuBFmZ7A3BatxaCbRjLIfz588aGfCUySpSu2SojPC9waSHIY2U7kK6eIp', NULL, '2020-12-15 00:17:46'),
+(9, '2', 'Author Name', 'author', 'generaluser@epac.com', NULL, NULL, NULL, '$2y$10$3EcxpGsHvehVa7ActoKD/eEizBsEKFkLNwlqYrqwzadhgJ2cEcFK6', 'default.png', NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoice_details`
+--
+ALTER TABLE `invoice_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `invoice_details`
+--
+ALTER TABLE `invoice_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
