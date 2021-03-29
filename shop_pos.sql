c068419df3ba156cd378c96cdc8fb3188951a772
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2021-03-29 22:52:52 +0600

    update

--- shop_pos.sql
+++ shop_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 04, 2021 at 12:27 PM
+-- Generation Time: Mar 29, 2021 at 04:49 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.13
 
